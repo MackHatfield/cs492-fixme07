a292055f8d7aef85eaf334db09111bf0df996324
Mackenzie Hatfield <mackhatfield@10-116-15-120.wireless.oregonstate.edu>
2019-10-23 09:04:18 -0700

    Actually add crimes when creating them to mCrimes in CrimeLab.

--- app/src/main/java/edu/osucascades/cs492/fixme07/CrimeLab.java
+++ app/src/main/java/edu/osucascades/cs492/fixme07/CrimeLab.java
@@ -24,6 +24,8 @@ public class CrimeLab {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
             crime.setSolved(i % 2 == 0);
+
+            mCrimes.add(crime);
         }
     }
 
