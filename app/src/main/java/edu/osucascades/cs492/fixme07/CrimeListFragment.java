8724c1bba00d8b9d3a5f15405631f9e667b32496
Mackenzie Hatfield <mackhatfield@10-116-15-120.wireless.oregonstate.edu>
2019-10-23 08:43:20 -0700

    Call method getCrimes on crimeLab created from singleton not class method.

--- app/src/main/java/edu/osucascades/cs492/fixme07/CrimeListFragment.java
+++ app/src/main/java/edu/osucascades/cs492/fixme07/CrimeListFragment.java
@@ -31,7 +31,7 @@ public class CrimeListFragment extends Fragment {
 
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
-        List<Crime> crimes = CrimeLab.getCrimes();
+        List<Crime> crimes = crimeLab.getCrimes();
 
         mAdapter = new CrimeAdapter(crimes);
         mCrimeRecyclerView.setAdapter(mAdapter);
